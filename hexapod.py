a5f2ec51398eb426063e5d6d65405e74cdeca4b3
Styku91 <brtstyczen@gmail.com>
2015-11-19 18:26:56 +0000

    calibration added

--- hexapod.py
+++ hexapod.py
@@ -1,8 +1,6 @@
 import numpy as np
 import pygame
 import XboxController
-import os, sys
-import threading
 import time
 import serial
 import math
@@ -20,11 +18,20 @@ class Joint(object):
         self.side = -1
         self.phase = 0
         self.centerRange = 500
+        self.calibration = 0
+        
     def changeCenter(self, c):
         if self.center + c <= self.absoluteCenter + self.centerRange and self.center + c >= self.absoluteCenter - self.centerRange:
             self.center += c*self.side
             self.maxPos = self.center + self.mvRange
             self.minPos = self.center - self.mvRange 
+
+    def getPosition(self):
+        return self.pos + self.calibration
+        
+    def calibrate(self, n):
+        if abs(self.calibration + n) < 200:
+            self.calibration += n
         
     def moveCenter(self):
         self.pos = self.center
@@ -37,8 +44,6 @@ class Joint(object):
             t = t%self.maxT
             self.pos = self.center + self.side*abs(self.mvRange*np.sin(2*(np.pi/self.maxT)*t))
 
-        return ("#%d P%d" % (self.port, self.pos))
-
     def moveFemur(self, t):
         if self.phase == 1:
             t += self.maxT/4
@@ -46,7 +51,6 @@ class Joint(object):
             t = t%self.maxT
             if (t <= self.maxT/4 or (t > self.maxT/2 and t <= (3*self.maxT)/4)):
                 self.pos = self.center + self.side*abs(self.mvRange*np.sin(4*(np.pi/(self.maxT))*t))
-        return ("#%d P%d" % (self.port, self.pos))
 
     def strafeTibia(self, t):
         if self.phase == 1:
@@ -54,8 +58,9 @@ class Joint(object):
         if self.phase == 0 or t >= self.maxT/2:
             t = t%self.maxT
             self.pos = self.center + self.side*abs(self.mvRange*np.sin(2*(np.pi/self.maxT)*t))
-
-        return ("#%d P%d" % (self.port, self.pos))
+    
+    def getCommand(self):
+        return ("#%d P%d" % (self.port, self.pos + self.calibration))
 
 class Leg(object):
     RIGHT = -1
@@ -113,33 +118,52 @@ class Hexapod(object):
 
     def moveForward(self, state):
         for i in range(6):
-            self.driver.sendCommand(self.legs[i].coxa.moveCoxa(state))
-            self.driver.sendCommand(self.legs[i].femur.moveFemur(state))
+            self.legs[i].coxa.moveCoxa(state)
+            self.legs[i].femur.moveFemur(state)
+            self.driver.sendCommand(self.legs[i].coxa.getCommand())
+            self.driver.sendCommand(self.legs[i].femur.getCommand())
 
     def strafeRight(self, state):
         for i in range(6):
-            self.driver.sendCommand(self.legs[i].tibia.strafeTibia(state))
-            self.driver.sendCommand(self.legs[i].femur.moveFemur(state))
+            self.legs[i].tibia.strafeTibia(state)
+            self.legs[i].femur.moveFemur(state)
+            self.driver.sendCommand(self.legs[i].tibia.getCommand())
+            self.driver.sendCommand(self.legs[i].femur.getCommand())
 
     def stand(self):
         for i in range(6):
-            self.driver.sendCommand(self.legs[i].coxa.moveCenter())
-            self.driver.sendCommand(self.legs[i].femur.moveCenter())
-            self.driver.sendCommand(self.legs[i].tibia.moveCenter())
+            self.legs[i].coxa.moveCenter()
+            self.legs[i].femur.moveCenter()
+            self.legs[i].tibia.moveCenter()
+            self.driver.sendCommand(self.legs[i].coxa.getCommand())
+            self.driver.sendCommand(self.legs[i].femur.getCommand())
+            self.driver.sendCommand(self.legs[i].tibia.getCommand())
+            
+    def calibrate(self, leg, c=0, f=0, t=0):
+        self.legs[leg].coxa.calibrate(c)
+        self.legs[leg].femur.calibrate(f)
+        self.legs[leg].tibia.calibrate(t)
+        self.driver.sendCommand(self.legs[leg].coxa.getCommand())
+        self.driver.sendCommand(self.legs[leg].femur.getCommand())
+        self.driver.sendCommand(self.legs[leg].tibia.getCommand())
     
     def up(self):
         for i in range(6):
             self.legs[i].tibia.changeCenter(-5)
             self.legs[i].femur.changeCenter(-5)
-            self.driver.sendCommand(self.legs[i].tibia.moveCenter())
-            self.driver.sendCommand(self.legs[i].femur.moveCenter())
+            self.legs[i].tibia.moveCenter()
+            self.legs[i].femur.moveCenter()
+            self.driver.sendCommand(self.legs[i].tibia.getCommand())
+            self.driver.sendCommand(self.legs[i].femur.getCommand())
 
     def down(self):
         for i in range(6):
             self.legs[i].tibia.changeCenter(5)
             self.legs[i].femur.changeCenter(5)
-            self.driver.sendCommand(self.legs[i].tibia.moveCenter())
-            self.driver.sendCommand(self.legs[i].femur.moveCenter())
+            self.legs[i].tibia.moveCenter()
+            self.legs[i].femur.moveCenter()
+            self.driver.sendCommand(self.legs[i].tibia.getCommand())
+            self.driver.sendCommand(self.legs[i].femur.getCommand())
 
 class ServoDriver(object):
     def __init__(self, devport="/dev/ttyACM0", devbaudrate=115200):
@@ -167,6 +191,13 @@ class Controller(object):
         TURN_RIGHT = 6
         UP = 7
         DOWN = 8
+        INCREASE = 9
+        DECREASE = 10
+        
+    class Mode():
+        STANDBY = 0
+        NORMAL = 1
+        CALIBRATION = 2
 
     def __init__(self):
         self.state = 0
@@ -176,37 +207,84 @@ class Controller(object):
         self.robot = Hexapod()
         self.driver = ServoDriver()
         self.robot.connectDriver(self.driver)
-        self.action = [True,False,False,False,False,False,False,False,False]
+        self.action = [True,False,False,False,False,False,False,False,False,False,False]
         self.pad = XboxController.XboxController(None, deadzone = 30, scale = 100, invertYAxis = True)
+        self.mode = self.Mode.NORMAL
+        self.calibration = 0;
+        self.loadCalibrationData()
+        
+    def saveCalibrationData(self):
+        f = open("calib.dat", "w+")   
+        for i in range(6):
+            f.write("%d\n" % self.robot.legs[i].coxa.calibration)
+            f.write("%d\n" % self.robot.legs[i].femur.calibration)
+            f.write("%d\n" % self.robot.legs[i].tibia.calibration)
+        f.close()
     
+    def loadCalibrationData(self):
+        f = open("calib.dat", "r")
+        lines = f.read().splitlines()
+        for i in range(6):
+            self.robot.legs[i].coxa.calibration = int(lines[i*3])
+            self.robot.legs[i].femur.calibration = int(lines[i*3+1])
+            self.robot.legs[i].tibia.calibration = int(lines[i*3+2])
+        f.close()
+        
     def getInput(self):
-        moved = False;
         for event in pygame.event.get():
-            print event.axis
-            print event.value
-            if event.type == 7: #left analog
-                if event.axis == 1:
+            print event
+            if event.type == 7:
+                if self.mode == self.Mode.NORMAL: #left analog
+                    if event.axis == 1:
+                        if event.value < -0.3:
+                            self.action[self.Action.FORWARD]=True
+                            self.speed = 2 if abs(event.value)>0.9 else 1
+                        elif event.value >= -0.3 and event.value <= 0.3:
+                            self.action[self.Action.FORWARD]=False
+                            self.speed = 0
+                        else:
+                            self.action[self.Action.FORWARD]=True
+                            self.speed = -2 if abs(event.value)>0.9 else -1
+                    elif event.axis ==5: #Right Trigger 
+                        if event.value > 0:
+                            self.action[self.Action.UP]=True
+                        else:
+                            self.action[self.Action.UP]=False
+    
+                    elif event.axis ==2: #Left Trigger 
+                        if event.value > 0:
+                            self.action[self.Action.DOWN]=True
+                        else:
+                            self.action[self.Action.DOWN]=False
+                elif self.mode == self.Mode.CALIBRATION:
                     if event.value < -0.3:
-                        self.action[self.Action.FORWARD]=True
+                        self.action[self.Action.INCREASE]=True
                         self.speed = 2 if abs(event.value)>0.9 else 1
                     elif event.value >= -0.3 and event.value <= 0.3:
                         self.action[self.Action.FORWARD]=False
                         self.speed = 0
                     else:
-                        self.action[self.Action.FORWARD]=True
+                        self.action[self.Action.DECREASE]=True
                         self.speed = -2 if abs(event.value)>0.9 else -1
-                elif event.axis ==5: #Right Trigger 
-                    if event.value > 0:
-                        self.action[self.Action.UP]=True
-                    else:
-                        self.action[self.Action.UP]=False
+            elif event.type == 11 and event.button == 6:
+                if self.mode != self.Mode.CALIBRATION: #select
+                    print "Calibration mode on"
+                    self.mode = self.Mode.CALIBRATION
+                    print self.mode
+                else:
+                    print "Calibration mode off"
+                    self.saveCalibrationData()
 
-                elif event.axis ==2: #Left Trigger 
-                    if event.value > 0:
-                        self.action[self.Action.DOWN]=True
+                    self.mode = self.Mode.NORMAL
+            elif event.type == 11 and event.button == 3:
+                if self.mode == self.Mode.CALIBRATION: #select
+                    if self.calibration == 17:
+                        self.calibration = 0
                     else:
-                        self.action[self.Action.DOWN]=False
-
+                        self.calibration += 1
+                    print "Limb %d selected" % self.calibration
+        
+                    
         if sum(self.action) == 0:
             self.action[self.Action.STAND] = True
 
@@ -215,20 +293,36 @@ class Controller(object):
         while True: 
             self.getInput()
 
-            if self.action[self.Action.FORWARD]:
-                self.state += self.speed
-                self.robot.moveForward(self.state)
-            elif self.action[self.Action.STRAFE_RIGHT]:
-                self.state += self.speed
-                self.robot.strafeRight(self.state)
-            elif self.action[self.Action.UP]:
-                self.robot.up()
-            elif self.action[self.Action.DOWN]:
-                self.robot.down()
-            elif self.action[self.Action.STAND]:
-                self.state = 0
-                self.robot.stand()
-            
+            if self.mode == self.Mode.NORMAL:
+                if self.action[self.Action.FORWARD]:
+                    self.state += self.speed
+                    self.robot.moveForward(self.state)
+                elif self.action[self.Action.STRAFE_RIGHT]:
+                    self.state += self.speed
+                    self.robot.strafeRight(self.state)
+                elif self.action[self.Action.UP]:
+                    self.robot.up()
+                elif self.action[self.Action.DOWN]:
+                    self.robot.down()
+                elif self.action[self.Action.STAND]:
+                    self.state = 0
+                    self.robot.stand()                
+            if self.mode == self.Mode.CALIBRATION:
+                if self.action[self.Action.INCREASE]:
+                    c = 0
+                    f = 0
+                    t = 0
+                    leg = int(self.calibration / 3)
+                    if self.calibration % 3 == 0:
+                        c = self.speed
+                    elif self.calibration % 3 == 1:
+                        f = self.speed
+                    else:
+                        t = self.speed
+                
+                    self.robot.calibrate(leg, c, f, t)
+                   
+                
             self.driver.executeCommand()
             #pygame.event.clear()
             time.sleep(0.020)
